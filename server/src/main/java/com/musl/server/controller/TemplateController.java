702719e42a6945bf8fbd7571a173ba9f4b70325f
Daniil <daniil.sharin667@gmail.com>
2025-02-08 23:59:17 +0200

    unfinished exercise to template adding functionality

--- server/src/main/java/com/musl/server/controller/TemplateController.java
+++ server/src/main/java/com/musl/server/controller/TemplateController.java
@@ -1,6 +1,8 @@
 package com.musl.server.controller;
 
+import com.musl.server.model.Exercise;
 import com.musl.server.model.Template;
+import com.musl.server.repository.ExerciseRepository;
 import com.musl.server.repository.TemplateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +14,27 @@ public class TemplateController {
     @Autowired
     private TemplateRepository templateRepository;
 
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
     @PostMapping(path = "/add")
     public @ResponseBody String addNewTemplate(@RequestBody Template template) {
+        Template newTemplate = new Template();
+        newTemplate.setTitle(template.getTitle());
+        newTemplate.setNotes(template.getNotes());
+        templateRepository.save(newTemplate);
+        return "Saved";
+    }
+
+    @PostMapping(path = "/addExercise")
+    public @ResponseBody String addExerciseToTemplate(
+            @RequestParam Long templateId,
+            @RequestParam Long exerciseId
+    ) {
+        Template template = templateRepository.findById(templateId)
+                .orElseThrow();
+        Exercise exercise = exerciseRepository.findById(exerciseId)
+                .orElseThrow();
         Template newTemplate = new Template();
         newTemplate.setTitle(template.getTitle());
         newTemplate.setNotes(template.getNotes());
